91dd82ea55cbe8fcaceeb272262cfe69b167a52a
Meng0251 <dreyu203@gmail.com>
2025-05-10 14:32:44 +0800

    Update lang.js

--- lang.js
+++ lang.js
@@ -5,6 +5,8 @@ const i18n = {
     intro1_text: "我是一位對創作、科技、語言有著極大熱情的學生，喜歡用影像、文字與程式探索這個世界。無論是做影片、寫詩、還是寫程式，我總是在尋找「怎麼讓大家都能輕鬆了解」。這個網站就是我創作歷程的記錄與延伸。",
     intro2_title: "未來方向",
     intro2_text: "我希望未來能結合科技與人文，創造具有影響力的內容。也許是一部講述科技焦慮的紀錄片，也許是一首記錄日常的歌，甚至是一個能幫助他人的時間管理 App。比起專注單一領域，我更在意不同能力交會時產生的火花。"
+    intro3_title: "使用的程式語言",
+    intro3_text: "我使用的程式語言包括C++ HTML 還有 PYTHON，並努力學習新的程式語言。"
   },
   en: {
     subtitle: "A student & creator who loves exploring and creating",
@@ -12,5 +14,7 @@ const i18n = {
     intro1_text: "I am a student passionate about creation, technology, and language. I explore the world through videos, writing, and code. Whether I'm making a video, writing a poem, or coding an app, I’m always trying to say in a way everyone can understand. This site is a record and extension of my creative journey.",
     intro2_title: "Future Vision",
     intro2_text: "In the future, I hope to merge technology and humanities to create impactful content. It could be a documentary about tech anxiety, a song about everyday life, or a time manage app that helps people. Rather than specializing in a single field, I value the sparks created when different skills intersect."
+    intro3_title: "Prograamming Languages I Use",
+    intro3_text: "The programming languages I use include C++, HTML, and Python, and I am working hard to learn new ones."  
   }
 };
