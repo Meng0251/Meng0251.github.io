fdd69d708239da812ab62fcc84a015f292b8f468
Meng0251 <dreyu203@gmail.com>
2026-08-30 17:08:24 +0800

    Delete lang.js

--- lang.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-  const bar = document.querySelector('.top-bar');
-  if (window.scrollY > 100) {
-    bar.style.transform = 'translateY(-100%)';
-  } else {
-    bar.style.transform = 'translateY(0)';
-  }
-});
